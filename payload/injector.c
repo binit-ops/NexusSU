a6b19a1957fba1e2205ecf980082915312db930c
Binit <jaiswalbinit605@gmail.com>
2026-06-30 21:00:46 +0530

    Update injector.c

--- payload/injector.c
+++ payload/injector.c
@@ -1,18 +1,16 @@
-struct nexus_cred {
-        int usage;
-            int uid; int gid;
-                int suid; int sgid;
-                    int euid; int egid;
-                        int fsuid; int fsgid;
-                        };
+struct nexus_su_cred {
+    int usage;
+    int uid; int gid;
+    int suid; int sgid;
+    int euid; int egid;
+    int fsuid; int fsgid;
+};
 
-                        void grant_nexus_root(struct nexus_cred *creds) {
-                            if (creds) {
-                                    creds->uid = 0;   creds->gid = 0;
-                                            creds->suid = 0;  creds->sgid = 0;
-                                                    creds->euid = 0;  creds->egid = 0;
-                                                            creds->fsuid = 0; creds->fsgid = 0;
-                                                                }
-                                                                }
-                                                                
+void grant_nexus_su_root(struct nexus_su_cred *creds) {
+    if (creds) {
+        creds->uid = 0;   creds->gid = 0;
+        creds->suid = 0;  creds->sgid = 0;
+        creds->euid = 0;  creds->egid = 0;
+        creds->fsuid = 0; creds->fsgid = 0;
+    }
 }
