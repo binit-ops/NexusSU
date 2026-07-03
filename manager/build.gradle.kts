e04bf8b63e8998717f4908e7fac461d87fc30c57
Binit <jaiswalbinit605@gmail.com>
2026-07-03 18:54:09 +0530

    Update build.gradle.kts

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.nexussu.manager"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.nexussu.manager"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
