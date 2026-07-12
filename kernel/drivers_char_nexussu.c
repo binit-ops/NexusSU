4a310a5482c552559a4314a67547aa32bb37d4a1
Binit <jaiswalbinit605@gmail.com>
2026-07-12 22:32:37 +0530

    Implement revoke UID functionality in NexusSU driver
    Added a new IOCTL command to revoke a trusted UID and updated the ioctl handler to support it.

--- kernel/drivers_char_nexussu.c
+++ kernel/drivers_char_nexussu.c
@@ -7,38 +7,33 @@
 #define NEXUSSU_IOC_MAGIC 'N'
 #define NEXUSSU_ALLOW_UID _IOW(NEXUSSU_IOC_MAGIC, 1, uid_t)
 #define NEXUSSU_GET_VERSION _IOR(NEXUSSU_IOC_MAGIC, 2, int)
-#define NEXUSSU_ESCALATE_SELF _IO(NEXUSSU_IOC_MAGIC, 3) // Allows Manager to escalate itself
+#define NEXUSSU_ESCALATE_SELF _IO(NEXUSSU_IOC_MAGIC, 3)
+#define NEXUSSU_REVOKE_UID _IOW(NEXUSSU_IOC_MAGIC, 4, uid_t) // NEW
 
 const int ENGINE_VERSION = 100;
 
-/* Handle incoming configuration requests from the Manager App */
 static long nexussu_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
     uid_t target_uid;
     
     switch (cmd) {
         case NEXUSSU_ALLOW_UID:
-            if (copy_from_user(&target_uid, (uid_t __user *)arg, sizeof(uid_t))) {
-                return -EFAULT;
-            }
+            if (copy_from_user(&target_uid, (uid_t __user *)arg, sizeof(uid_t))) return -EFAULT;
             nexussu_add_trusted_uid(target_uid);
-            printk(KERN_INFO "NexusSU: Added UID %d to allowlist\n", target_uid);
+            break;
+            
+        case NEXUSSU_REVOKE_UID: // NEW
+            if (copy_from_user(&target_uid, (uid_t __user *)arg, sizeof(uid_t))) return -EFAULT;
+            nexussu_revoke_trusted_uid(target_uid);
+            printk(KERN_INFO "NexusSU: Revoked UID %d\n", target_uid);
             break;
             
         case NEXUSSU_GET_VERSION:
-            if (copy_to_user((int __user *)arg, &ENGINE_VERSION, sizeof(int))) {
-                return -EFAULT;
-            }
+            if (copy_to_user((int __user *)arg, &ENGINE_VERSION, sizeof(int))) return -EFAULT;
             break;
 
         case NEXUSSU_ESCALATE_SELF:
-            /* 
-             * REAL ROOT: The Manager App calls this to escalate itself to UID 0.
-             * This breaks the Catch-22: The manager needs root to mount the 'su' binary,
-             * but needs the 'su' binary to get root.
-             */
             nexussu_escalate();
-            printk(KERN_INFO "NexusSU: Manager App escalated self to root.\n");
             break;
             
         default:
