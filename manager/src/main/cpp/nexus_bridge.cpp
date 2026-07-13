dabf299ca45f83e46d804ed75d72454ed583bb1a
Binit <jaiswalbinit605@gmail.com>
2026-07-13 20:32:44 +0530

    Refactor NexusBridge to use prctl for UID management

--- manager/src/main/cpp/nexus_bridge.cpp
+++ manager/src/main/cpp/nexus_bridge.cpp
@@ -1,61 +1,54 @@
 #include <jni.h>
 #include <string>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
+#include <sys/prctl.h>
 #include <android/log.h>
 
-#define NEXUSSU_IOC_MAGIC 'N'
-#define NEXUSSU_ALLOW_UID _IOW(NEXUSSU_IOC_MAGIC, 1, uid_t)
-#define NEXUSSU_GET_VERSION _IOR(NEXUSSU_IOC_MAGIC, 2, int)
-#define NEXUSSU_ESCALATE_SELF _IO(NEXUSSU_IOC_MAGIC, 3)
-#define NEXUSSU_REVOKE_UID _IOW(NEXUSSU_IOC_MAGIC, 4, uid_t)
+#define NEXUSSU_PRCTL_MAGIC 0x4E535553
+#define CMD_GRANT_UID 1
+#define CMD_REVOKE_UID 2
+#define CMD_ESCALATE_SELF 3
+#define CMD_GET_VERSION 4
+#define CMD_REGISTER_MANAGER 5
 
 #define LOG_TAG "NexusBridge"
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_nexussu_manager_core_NexusEngine_registerManager(JNIEnv *env, jobject thiz) {
+    int ret = prctl(NEXUSSU_PRCTL_MAGIC, CMD_REGISTER_MANAGER, 0, 0, 0);
+    return ret == 0 ? JNI_TRUE : JNI_FALSE;
+}
 
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_nexussu_manager_core_NexusEngine_grantUidAccess(JNIEnv *env, jobject thiz, jint uid) {
-    int fd = open("/dev/nexussu", O_RDWR);
-    if (fd < 0) return JNI_FALSE;
-    uid_t target_uid = static_cast<uid_t>(uid);
-    int result = ioctl(fd, NEXUSSU_ALLOW_UID, &target_uid);
-    close(fd);
-    return result == 0 ? JNI_TRUE : JNI_FALSE;
+    int ret = prctl(NEXUSSU_PRCTL_MAGIC, CMD_GRANT_UID, (unsigned long)uid, 0, 0);
+    return ret == 0 ? JNI_TRUE : JNI_FALSE;
 }
 
-// NEW: Revoke JNI
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_nexussu_manager_core_NexusEngine_revokeUidAccess(JNIEnv *env, jobject thiz, jint uid) {
-    int fd = open("/dev/nexussu", O_RDWR);
-    if (fd < 0) return JNI_FALSE;
-    uid_t target_uid = static_cast<uid_t>(uid);
-    int result = ioctl(fd, NEXUSSU_REVOKE_UID, &target_uid);
-    close(fd);
-    return result == 0 ? JNI_TRUE : JNI_FALSE;
-}
-
-extern "C"
-JNIEXPORT jint JNICALL
-Java_com_nexussu_manager_core_NexusEngine_getEngineVersion(JNIEnv *env, jobject thiz) {
-    int fd = open("/dev/nexussu", O_RDONLY);
-    if (fd < 0) return -1;
-    int version = -1;
-    int result = ioctl(fd, NEXUSSU_GET_VERSION, &version);
-    close(fd);
-    if (result < 0) return -1;
-    return version;
+    int ret = prctl(NEXUSSU_PRCTL_MAGIC, CMD_REVOKE_UID, (unsigned long)uid, 0, 0);
+    return ret == 0 ? JNI_TRUE : JNI_FALSE;
 }
 
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_nexussu_manager_core_NexusEngine_escalateSelf(JNIEnv *env, jobject thiz) {
-    int fd = open("/dev/nexussu", O_RDWR);
-    if (fd < 0) return JNI_FALSE;
-    int result = ioctl(fd, NEXUSSU_ESCALATE_SELF);
-    close(fd);
-    return result == 0 ? JNI_TRUE : JNI_FALSE;
+    int ret = prctl(NEXUSSU_PRCTL_MAGIC, CMD_ESCALATE_SELF, 0, 0, 0);
+    if (ret == 0) {
+        LOGI("NexusSU Manager escalated to root via prctl");
+        return JNI_TRUE;
+    }
+    LOGE("prctl escalation failed");
+    return JNI_FALSE;
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_nexussu_manager_core_NexusEngine_getEngineVersion(JNIEnv *env, jobject thiz) {
+    return prctl(NEXUSSU_PRCTL_MAGIC, CMD_GET_VERSION, 0, 0, 0);
 }
