96e9c821fca65b1dd9b66751135acbc2d8e64896
Binit <jaiswalbinit605@gmail.com>
2026-07-14 12:29:16 +0530

    Add missing import for items in Screens.kt

--- manager/src/main/java/com/nexussu/manager/ui/Screens.kt
+++ manager/src/main/java/com/nexussu/manager/ui/Screens.kt
@@ -24,7 +24,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
