f5610ea9c794bb55caac151d23185916c774992b
Binit <jaiswalbinit605@gmail.com>
2026-07-16 22:27:39 +0530

    Create NexusApplication class for application setup

---
+++ manager/src/main/java/com/nexussu/manager/NexusApplication.kt
@@ -0,0 +1,10 @@
+package com.nexussu.manager
+
+import android.app.Application
+
+class NexusApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialization code if needed in the future
+    }
+}
